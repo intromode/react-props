d4a4069bcdc856de11843790510f828c09d84680
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-05-14 13:41:29 -0700

    incrementer rendering to browser

--- src/components/Incrementer.js
+++ src/components/Incrementer.js
@@ -1,2 +1,25 @@
-// import React, { } from 'react';
+import React, { PureComponent } from 'react';
+
+export default class Incrementer extends PureComponent {
+  state = {
+    buttonClicked: 0
+  }
+
+  eventHandler = () => {
+    this.setState(state => {
+      return {
+        buttonClicked: state.buttonClicked + 1
+      };
+    });
+  }
+
+  render() {
+    console.log(this.state.buttonClicked);
+
+    return <>
+      <h2>{this.state.buttonClicked}</h2>
+      <button onClick={this.eventHandler}>Increment</button>
+    </>;
+  }
+}
 
